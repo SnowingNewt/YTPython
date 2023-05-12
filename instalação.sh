db238fe985e3d7947abb566baef62c57c6d0c6ef
IKFGMS <91566884+SnowingNewt@users.noreply.github.com>
2023-05-12 08:05:59 -0300

    Create instalação.sh

---
+++ instalação.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#Baixa o repositorio
+git clone https://github.com/SnowingNewt/YTPython
+
+
+#Instala as dependencias
+pip3 install pytube
+
+#Entra na pasta
+cd YTPython
+
+#Executa a ferramenta
+python3 YTPython02.py
